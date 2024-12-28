30ee1967201028d8124fb6a031c28c7519c58a8b
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2024-12-28 11:31:16 -0300

    new port updationg

--- car_part_store_api/server.js
+++ car_part_store_api/server.js
@@ -1,7 +1,9 @@
 
 import app from './src/app.js'
 
+// app.listen(3000, () =>{
+//     console.log('Running...')
+// })
 
-app.listen(3000, () =>{
-    console.log('Running...')
-})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Servidor rodando na porta ${port}`));

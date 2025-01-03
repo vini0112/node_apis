82954c756f21c455ed461e83e510bb05b9e71058
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-01-03 19:15:59 -0300

    .

--- src/app.js
+++ src/app.js
@@ -8,11 +8,9 @@ const app = express()
 
 app.use(cors())
 app.use(express.json())
-app.use('/uploads', express.static('uploads'))
-
 app.use(router)
 app.use(usersRouter)
-
+app.use('/uploads', express.static('uploads'))
 
 
 export default app

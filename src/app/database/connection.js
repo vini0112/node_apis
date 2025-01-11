84d635a18d48f2a7d37eb04dd5f791f8671baa82
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-01-11 15:01:20 -0300

    trying connection

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -1,13 +1,21 @@
 import mysql from 'mysql2'
 
 
+// const connection = mysql.createConnection({
+//     host: 'localhost',
+//     host: process.env.DATABASE_URL,
+//     port: '3306',
+//     user: 'root',
+//     password: 'vini1@',
+//     database: 'store_parts'
+// })
+
 const connection = mysql.createConnection({
-    // host: 'localhost',
-    host: process.env.DATABASE_URL,
-    port: '3306',
-    user: 'root',
-    password: 'vini1@',
-    database: 'store_parts'
+    host: process.env.MYSQL_HOST,
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
+    database: process.env.MYSQL_DATABASE,
+    port: process.env.MYSQL_PORT,
 })
 
 

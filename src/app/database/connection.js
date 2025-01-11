df81067cb391627af91a7ee9230ad7f31f14e4a0
Vinicius Silva <159788677+vini0112@users.noreply.github.com>
2025-01-11 15:11:05 -0300

    .

--- src/app/database/connection.js
+++ src/app/database/connection.js
@@ -11,11 +11,11 @@ import mysql from 'mysql2'
 // })
 
 const connection = mysql.createConnection({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
+    host: process.env.MYSQLHOST,
+    user: process.env.DB_USER,
+    password: process.env.MYSQLPASSWORD,
     database: process.env.MYSQL_DATABASE,
-    port: process.env.MYSQL_PORT,
+    port: process.env.DB_PORT,
 })
 
 
